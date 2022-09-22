298ce2efd114361ca4cdd1ac650237b517870829
Medas <medas02345@gmail.com>
2022-09-22 15:14:39 +0300

    Create a POST function which omits the selected letter

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.post("/omit", (req, res) => {
+  const { initialString, removableLetter } = req.body;
+  const removableLetterUpperCase = removableLetter.toUpperCase();
+  const removableLetterLowerCase = removableLetter.toLowerCase();
+  const omittedString = initialString
+    .replace(new RegExp(removableLetterLowerCase, "g"), "")
+    .replace(new RegExp(removableLetterUpperCase, "g"), "");
+  res.send(omittedString);
+});
+
 app.listen(5000, () => {
   console.log(`Server is running on port 5000.`);
 });
