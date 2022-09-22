538f8bfe9b087a2ae8c9de8875b435f523ead019
Medas <medas02345@gmail.com>
2022-09-22 15:50:58 +0300

    Change port to environment instead of 5000

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const port = process.env.PORT || 5000
 
 app.use(bodyParser.json());
 
@@ -19,6 +20,6 @@ app.post("/omit", (req, res) => {
   res.send(omittedString);
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port 5000.`);
 });
